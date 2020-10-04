529b6756fdeced9ddd36deae691539cbe145044e
bennysuhardjo <71486494+bennysuhardjo@users.noreply.github.com>
2020-10-04 13:28:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def display_page(pathname):
     headerscredit = {
         'clientId': "c205ebf1-c7d7-4bf5-bc18-1af048aafa8f",
         'accessToken': response.text.split(',')[0].split(':')[1].split('"')[1],
-        'uuid': "b7ab8a39-1e22-4ddf-82c9-a2ec70052c9a"
+        'uuid': "IW036"
         
     }
 
