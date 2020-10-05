2c5502d85a2492d1a79b9444d8252d3a797ae5b8
bennysuhardjo <71486494+bennysuhardjo@users.noreply.github.com>
2020-10-05 13:19:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def display_page(pathname):
                                   headers={'clientId': 'c205ebf1-c7d7-4bf5-bc18-1af048aafa8f',
                                            'accessToken': response.text.split(',')[0].split(':')[1].split('"')[1]
                                            ,
-                                           'uuid': "IW038"
+                                           'accept': "application/json"
                                           })
     
     return html.Div([
